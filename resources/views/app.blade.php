1890b5a71deb6bb0193370caed9270d55cecddb5
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-08-30 19:09:26 +0700

    section landing page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -94,7 +94,45 @@
                 </div>
             </div>
         </div>
+            <!-- ======= Hero Section ======= -->
+            <section id="hero" class="d-flex justify-content-center align-items-center">
+                <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+                <h1>BPPD,<br>Layanan Pajak</h1>
+                <h2>Kota Bandung</h2>
+                <a class="btn-get-started" data-bs-toggle="modal" data-bs-target="#login">Bayar Sekarang !!!</a>
+                                </div>
+            </section><!-- End Hero -->
+                <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
 
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <img src="assets/landing/img/s.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            </p>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+  <main id="main">
+        
         <div class="modal fade" id="login" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
