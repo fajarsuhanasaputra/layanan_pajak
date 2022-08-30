158dc605b1819e42ca54227cd2048cf56812ee31
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-08-31 00:25:38 +0700

    setup admin template and dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,35 @@
-Welcome dashboard!
+@extends('dashboard.layout')
+@section('content')
+<div class="row">
+    <div class="col-lg-2 col-md-6 col-sm-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-bar-chart-o fa-fw"></i> Total Riwayat Pembayaran
+            </div>
+            <div class="panel-body text-center">
+                <h3 class="h1">5</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2 col-md-6 col-sm-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-bar-chart-o fa-fw"></i> Jenis Pajak A
+            </div>
+            <div class="panel-body text-center">
+                <h3 class="h1">2</h3>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-2 col-md-6 col-sm-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-bar-chart-o fa-fw"></i> Jenis Pajak B
+            </div>
+            <div class="panel-body text-center">
+                <h3 class="h1">3</h3>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
