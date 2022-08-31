4ca94f8493a2549e5b6a48518af2a6e222be8de6
fajarsuhanasaputra <fajarsuhanaaa@gmail.com>
2022-08-31 08:05:58 +0700

    seeder admin

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+        'name' => 'Admin',
+        'role' => 'admin',
+        'username' => 'admin',
+        'password' => Hash::make('12345'),
+        ]);
+    }
+}
